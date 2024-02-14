0df2fb01c35a5c49bf987a860760ffef8f28883d
rdnbrs <barisrdn@gmail.com>
2024-02-14 18:55:49 +0300

    fix

--- src/main/java/com/rdnbrs/reflectiondemo/service/LogService.java
+++ src/main/java/com/rdnbrs/reflectiondemo/service/LogService.java
@@ -4,7 +4,6 @@ import com.rdnbrs.reflectiondemo.repository.TestRepository;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -12,12 +11,11 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class LogService {
 
-    private final ApplicationContext applicationContext;
+    private final TestRepository repository;
 
     @SneakyThrows
     public void logInfo(String string) {
         log.info(string);
-        TestRepository repositoryBean = (TestRepository) applicationContext.getBean("testRepository");
-        repositoryBean.findAll();
+        repository.findAll();
     }
 }
