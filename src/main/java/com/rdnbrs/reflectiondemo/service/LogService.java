bd92af6b41ea715edc41ca1097c51ffb1e997039
rdnbrs <barisrdn@gmail.com>
2024-02-14 18:31:28 +0300

    remove imports

--- src/main/java/com/rdnbrs/reflectiondemo/service/LogService.java
+++ src/main/java/com/rdnbrs/reflectiondemo/service/LogService.java
@@ -1,15 +1,11 @@
 package com.rdnbrs.reflectiondemo.service;
 
-import com.rdnbrs.reflectiondemo.entity.TestEntity;
 import com.rdnbrs.reflectiondemo.repository.TestRepository;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Method;
-import java.util.List;
 
 @Slf4j
 @Component
